cd21029debc80a9f5e511804beccb83c246a5c66
Rizki & Flowren <rizkifiananda@gmail.com>
2024-09-22 21:38:07 +0700

    add system joining chat

--- djangochat/settings.py
+++ djangochat/settings.py
@@ -64,6 +64,12 @@ TEMPLATES = [
 WSGI_APPLICATION = 'djangochat.wsgi.application'
 ASGI_APPLICATION = 'djangochat.asgi.application'
 
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels.layers.InMemoryChannelLayer'
+    }
+}
+
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
